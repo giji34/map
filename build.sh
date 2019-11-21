8d4136bff1c4b342bf7838707b6f7d19291f7614
kbinani <kbinani.bt@gmail.com>
2019-11-22 04:00:00 +0900

    fix: 処理の順番が適当過ぎた

--- build.sh
+++ build.sh
@@ -2,15 +2,17 @@
 
 set -eu
 
+(cd "$BACKUP_DIR" && git pull --ff-only origin master)
+
 yarn landmarks
 
 rm -rf images/0
-(cd "$BACKUP_DIR" && git pull --ff-only origin master)
 mkdir -p images/0
 ../mca2png/build/mca2png -w "$BACKUP_DIR/world" -o images/0 -l ./landmarks.tsv
+
 rm -rf public/images/0
 mkdir -p public/images/0
-
 yarn imagemin
+
 yarn imagelist
 yarn webpack
