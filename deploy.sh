7e66d0f680db47b6160d717027366441ab08e571
kbinani <kbinani.bt@gmail.com>
2019-12-02 04:11:08 +0900

    fix: max-age をワールドのバックアップ間隔と同じにする

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws s3 sync public s3://map.giji34.world --profile giji34 --exact-timestamps --delete --cache-control 'max-age=86400' --exclude "*.js.map"
+aws s3 sync public s3://map.giji34.world --profile giji34 --exact-timestamps --delete --cache-control 'max-age=900' --exclude "*.js.map"
