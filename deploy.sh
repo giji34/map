6744732e28700063cbf0a10866909c55d3dea1cc
kbinani <kbinani.bt@gmail.com>
2019-12-06 04:33:17 +0900

    feat: キャッシュさせるのは画像ファイルだけにする

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-aws s3 sync public s3://map.giji34.world --profile giji34 --exact-timestamps --delete --cache-control 'max-age=900' --exclude "*.js.map"
+aws s3 sync public/images s3://map.giji34.world/images --profile giji34 --exact-timestamps --delete --cache-control 'max-age=900'
+aws s3 sync public s3://map.giji34.world --profile giji34 --exact-timestamps --delete --cache-control 'no-store' --exclude "*.js.map" --exclude "images"
