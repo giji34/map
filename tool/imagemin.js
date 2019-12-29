cfd8d1bdc724a8bb76a1a8c7dbec3c3cccba0c71
kbinani <kbinani.bt@gmail.com>
2019-12-30 00:58:55 +0900

    feat: ディメンジョン毎の imagemin が同時に起動しないようにする

--- tool/imagemin.js
+++ tool/imagemin.js
@@ -1,11 +1,12 @@
 const imagemin = require('imagemin');
 const imageminZopfli = require('imagemin-zopfli');
 
-['o', 'n', 'e'].forEach(it => {
-    imagemin([`images/${it}/*.png`], {
-        destination: `public/images/${it}`,
-        plugins: [
-            imageminZopfli({more: true})
-        ]
-    });
+imagemin([`images/o/*.png`], {destination: `public/images/o`, plugins: [imageminZopfli({more: true})]}).then(() => {
+	console.log(`finished: overworld`);
+	imagemin([`images/n/*.png`], {destination: `public/images/n`, plugins: [imageminZopfli({more: true})]}).then(() => {
+		console.log(`finished: nether`);
+		imagemin([`images/e/*.png`], {destination: `public/images/e`, plugins: [imageminZopfli({more: true})]}).then(() => {
+			console.log(`finished: the end`);
+		});
+	});
 });
