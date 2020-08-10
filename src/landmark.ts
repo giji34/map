7e21db5db6fcfbb7882f1dc0d80990b5d3cc78d1
kbinani <kbinani.bt@gmail.com>
2020-08-10 18:39:08 +0900

    cleanup: Point, Point3 を points.ts に移動

--- src/landmark.ts
+++ src/landmark.ts
@@ -1,34 +1,5 @@
 import { romanize } from "./romanize";
-
-export class Point {
-  readonly x: number;
-  readonly z: number;
-
-  constructor(x: number, z: number) {
-    this.x = x;
-    this.z = z;
-  }
-
-  toString() {
-    return `[${this.x},${this.z}]`;
-  }
-
-  clone(): Point {
-    return new Point(this.x, this.z);
-  }
-
-  static distance(a: Point, b: Point): number {
-    return Math.hypot(a.x - b.x, a.z - b.z);
-  }
-}
-
-export class Point3 {
-  constructor(readonly x: number, readonly y: number, readonly z: number) {}
-
-  clone(): Point3 {
-    return new Point3(this.x, this.y, this.z);
-  }
-}
+import { Point, Point3 } from "./point";
 
 export enum Dimension {
   TheNether = -1,
